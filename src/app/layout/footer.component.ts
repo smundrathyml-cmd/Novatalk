44959aa9df5af44983566fa016648a35fdc38d9b
Susheel <smundrathy.ml@gmail.com>
2025-09-09 17:57:44 +0530

    Footer logo

--- src/app/layout/footer.component.ts
+++ src/app/layout/footer.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-@Component({ selector:'app-footer', standalone:true, imports:[CommonModule,RouterModule], templateUrl:'./footer.component.html' })
+@Component({ selector:'app-footer', standalone:true, imports:[CommonModule,RouterModule], templateUrl:'./footer.component.html', styleUrls:['./footer.component.css'] })
 export class FooterComponent{}
